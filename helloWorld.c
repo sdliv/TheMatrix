559d87136c55bdb98848ec4656e901d65c5e4041
pleiromatics <pleiromatics@Seans-MacBook-Pro-2.local>
2016-10-11 13:41:24 -0400

    C: learning about memory, assembly, buffers, stacks, heap, etc.

---
+++ helloWorld.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	int a = 9; int b = 0; int c = 4; int d = 2; int sum; int joannaAge;
+	sum = a + d + c;
+	joannaAge = c + d;
+	printf("Hello Joanna. I hear you like to watch Puffin' Rock?\n And you like to watch Magic School bus too?\n and you like to follow Jesus?\n Joanna is %d years old.", joannaAge);
+	return 0;
+}
